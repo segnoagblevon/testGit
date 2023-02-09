59cc5d95201e9fed6716cf106e0931335259d85a
segnoagblevon <segnoagblevon@gmail.com>
2023-02-09 09:42:15 +0000

    changes ok

---
+++ TestModule.py
@@ -0,0 +1,34 @@
+
+
+def multiply(self,first=1, second=1):
+    '''Multiply 2 nombers.
+        :param first: -- the multiplicand
+        :param second: -- the multiplier (défaut 1)
+        :return ""
+    '''
+    return first * second
+
+def handle(self, Param1, Param2, Param3):
+    """title of docstring
+    :param int Param1: first parameter
+    :param string Param2: second parameter
+    :param float Param3: third parameter
+
+    :return: name of sum
+    """
+def showName(name):
+    """
+
+    :param name:
+    :return:
+    """
+
+
+
+    print(multiply(13, 77))
+
+    #    print(multiply.__doc__)
+
+    #help(TestModule)
+
+    multiply()
